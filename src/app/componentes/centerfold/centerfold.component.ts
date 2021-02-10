d270b8eb0715ca669dcb26b16a7a722a89fa1fdb
claudio sesto <claudiosesto62@gmail.com>
2021-02-10 13:22:47 -0300

    con selector

--- src/app/componentes/centerfold/centerfold.component.ts
+++ src/app/componentes/centerfold/centerfold.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,41 +7,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./centerfold.component.css']
 })
 export class CenterfoldComponent implements OnInit {
+
+  oldEvent: HTMLElement = null
+  bsqStr: string = ""
+  selector: number = 0;
+  
  heroes = [
-    {id: 1, categoria:'Zapatilla', color:'blanco', talle:'36',
+    {id: 1, categoria:'Zapatillas', color:'Blanco', talle:'36',
      url:'../assets/productos/zapatilla_1.jpg',precio:'5000', desc:'1-ZFK'},
-    {id: 2, categoria:'Botas', color:'blanco', talle:'38', 
+    {id: 2, categoria:'Botas', color:'Negro', talle:'38', 
     url:'../assets/productos/zapatilla_2.jpg',precio:'5000', desc:'2000-X'},
-    {id: 5, categoria:'Zapatilla', color:'blanco', talle:'40', 
+     {id: 3, categoria:'Zapatillas', color:'Verde', talle:'42', 
+    url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'1-ZFK'},
+    {id: 4, categoria:'Zapatillas', color:'Blanco', talle:'44', 
+    url:'../assets/productos/zapatilla_1.jpg' ,precio:'5000', desc:'1-ZFK'},
+    {id: 5, categoria:'Zapatillas', color:'Amarillo', talle:'40', 
     url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'3 Run'},
-    {id: 3, categoria:'Zapatilla', color:'blanco', talle:'42', 
+    {id: 1, categoria:'Zapatillas', color:'Blanco', talle:'36',
     url:'../assets/productos/zapatilla_1.jpg',precio:'5000', desc:'1-ZFK'},
-    {id: 4, categoria:'Zapatilla', color:'blanco', talle:'44', 
-    url:'../assets/productos/zapatilla_2.jpg' ,precio:'5000', desc:'1-ZFK'},
-    {id: 1, categoria:'Zapatilla', color:'blanco', talle:'36',
-    url:'../assets/productos/zapatilla_1.jpg',precio:'5000', desc:'1-ZFK'},
-   {id: 2, categoria:'Botas', color:'blanco', talle:'38', 
+   {id: 2, categoria:'Botas', color:'Negro', talle:'38', 
    url:'../assets/productos/zapatilla_2.jpg',precio:'5000', desc:'2000-X'},
-   {id: 5, categoria:'Zapatilla', color:'blanco', talle:'40', 
-   url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'3 Run'},
-   {id: 3, categoria:'Zapatilla', color:'blanco', talle:'42', 
+    {id: 3, categoria:'Zapatillas', color:'Verde', talle:'42', 
+   url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'1-ZFK'},
+   {id: 4, categoria:'Zapatillas', color:'Blanco', talle:'44', 
+   url:'../assets/productos/zapatilla_1.jpg' ,precio:'5000', desc:'1-ZFK'},
+   {id: 5, categoria:'Zapatillas', color:'Amarillo', talle:'40', 
+   url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'3 Run'},    {id: 1, categoria:'Zapatillas', color:'Blanco', talle:'36',
    url:'../assets/productos/zapatilla_1.jpg',precio:'5000', desc:'1-ZFK'},
-   {id: 4, categoria:'Zapatilla', color:'blanco', talle:'44', 
-   url:'../assets/productos/zapatilla_2.jpg' ,precio:'5000', desc:'1-ZFK'},
-   {id: 1, categoria:'Zapatilla', color:'blanco', talle:'36',
-   url:'../assets/productos/zapatilla_1.jpg',precio:'5000', desc:'1-ZFK'},
-  {id: 2, categoria:'Botas', color:'blanco', talle:'38', 
+  {id: 2, categoria:'Botas', color:'Negro', talle:'38', 
   url:'../assets/productos/zapatilla_2.jpg',precio:'5000', desc:'2000-X'},
-  {id: 5, categoria:'Zapatilla', color:'blanco', talle:'40', 
+   {id: 3, categoria:'Zapatillas', color:'Verde', talle:'42', 
+  url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'1-ZFK'},
+  {id: 4, categoria:'Zapatillas', color:'Blanco', talle:'44', 
+  url:'../assets/productos/zapatilla_1.jpg' ,precio:'5000', desc:'1-ZFK'},
+  {id: 5, categoria:'Zapatillas', color:'Amarillo', talle:'40', 
   url:'../assets/productos/zapatilla_3.jpg',precio:'5000', desc:'3 Run'},
-  {id: 3, categoria:'Zapatilla', color:'blanco', talle:'42', 
-  url:'../assets/productos/zapatilla_1.jpg',precio:'5000', desc:'1-ZFK'},
-  {id: 4, categoria:'Zapatilla', color:'blanco', talle:'44', 
-  url:'../assets/productos/zapatilla_2.jpg' ,precio:'5000', desc:'1-ZFK'}
 ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+selectCategoria(e: Event, filtra: number){
+const tar = e.target as HTMLElement // genial el cambio de tipo
+this.bsqStr= tar.innerText.toLowerCase()
+this.selector = filtra
+console.log('filtrando por : ' + this.bsqStr)
+if (tar == this.oldEvent){
+  this.oldEvent.style.backgroundColor = ''
+  this.selector = 0
 }
+this.getElementos()
+
+tar.style.backgroundColor = 'lightgray';
+  if(this.oldEvent != null){
+    this.oldEvent.style.backgroundColor = ''
+  //  this.oldEvent = null
+  }
+
+  this.oldEvent = tar
+ // this.selector = 0
+} 
+
+
+getElementos(){
+switch (this.selector){
+    case 1: {
+      let filtrado = this.heroes.filter(xx => xx.categoria.toLowerCase() === this.bsqStr)
+      return filtrado
+      break;
+    }
+    case 2: {
+      let filtrado =this.heroes.filter(xx => xx.talle === this.bsqStr)
+      return filtrado
+      break;
+    }
+    case 3: {
+   
+      let filtrado = this.heroes.filter(xx => xx.color.toLowerCase() === this.bsqStr)
+      return filtrado
+      break;
+    }
+    
+    default: {
+     let filtrado = this.heroes
+      return filtrado 
+      break;
+    } 
+}
+ 
+}
+}
+
+
